db514cb2e8d0e3017098ec0b74038097a37fd38d
Daniel <daniel.schreurs@hotmail.com>
2015-11-02 19:10:53 +0100

    add cours

--- beta/1.0.0/resources/views/partials/nav/teacher_nav.blade.php
+++ beta/1.0.0/resources/views/partials/nav/teacher_nav.blade.php
@@ -1,6 +1,7 @@
 <div role="navigation">
     {!! link_to('/','Mes cours',['class'=>'','title'=>'Renvoie vers le paneaux de controle des cours']) !!}
     {!! link_to('/','Mes Élèves',['class'=>'','title'=>'Renvoie vers le paneaux de controle des élèves']) !!}
+    {!! link_to('/cours','Mes cours',['class'=>'','title'=>'Renvoie vers le paneaux de controle des cours']) !!}
     {!! link_to('/','Mes collègues',['class'=>'','title'=>'Renvoie vers le paneaux de controle des collègues']) !!}
     {!! link_to('/','Les Locaux',['class'=>'','title'=>'Renvoie vers le paneaux de controle des locaux']) !!}
     {!! link_to('/','Mes groupes',['class'=>'','title'=>'Renvoie vers le paneaux de controle des groupes']) !!}
--- beta/1.0.0/resources/views/teacher/cours.blade.php
+++ beta/1.0.0/resources/views/teacher/cours.blade.php
@@ -1,51 +1,6 @@
 @extends('layouts.teacher_layout')
 @section('teacher_content')
 <ul>
-    <li>
-        <h2>Mes cours d‘aujourd'hui</h2>
-        <ul>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-        </ul>
-    </li>
-    <li>
-        <h2>Mes élèves</h2>
-        <ul>
-            <li>Armound Adeline</li>
-            <li>Blisntin Stéphan</li>
-            <li>Crutzen Marie</li>
-            <li>Delayen Estelle</li>
-            <li>Gérard Sylvain</li>
-        </ul>
-    </li>
-    <li>
-        <h2>Mes collègues</h2>
-        <ul>
-            <li>Armound Adeline</li>
-            <li>Blisntin Stéphan</li>
-            <li>Crutzen Marie</li>
-            <li>Delayen Estelle</li>
-            <li>Gérard Sylvain</li>
-        </ul>
-    </li>
-    <li>
-        <h2>Mes groupes</h2>
-        <ul>
-            <li>Armound Adeline</li>
-            <li>Blisntin Stéphan</li>
-            <li>Crutzen Marie</li>
-            <li>Delayen Estelle</li>
-            <li>Gérard Sylvain</li>
-        </ul>
-    </li>
-    <li>
-        <h2>Mon compte</h2>
-        <p>Didier Lionel</p>
-        <p>15 heures de cours</p>
-        <p>200 élèves</p>
-    </li>
+    <li>Charle</li>
 </ul>
 @stop
