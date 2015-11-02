2ecec05ffd5298ff2e94ca4b2b1225a7abb3f985
Daniel <daniel.schreurs@hotmail.com>
2015-11-02 10:55:59 +0100

    add routes

--- beta/1.0.0/app/Http/routes.php
+++ beta/1.0.0/app/Http/routes.php
@@ -1,16 +1,30 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Application Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register all of the routes for an application.
+    | It's a breeze. Simply tell Laravel the URIs it should respond to
+    | and give it the controller to call when that URI is requested.
+    |
+    */
 
-Route::get('/', function () {
-    return view('visitors.home');
-});
+    /*
+     * visistors
+     * */
+    Route::get('/school/register', function () {
+        return view('visitors.home');
+    });
+    Route::get('/user/login', function () {
+        return view('visitors.login');
+    });
+
+    Route::get('/',function(){
+       return view('dashboard.dashboard');
+    });
+     /*
+     * Aplication
+     */
+
---
+++ beta/1.0.0/resources/views/forms/users/login.blade.php
@@ -0,0 +1,21 @@
+{!! Form::open(['url' => '/']) !!}
+<fieldset>
+    <legend>Vos coordonnées</legend>
+    {!! Form::text('name','Votre prénom') !!}
+    {!! Form::text('name',old('name'),['class'=>'']) !!}
+    {!! Form::text('surname','Votre nom de famille') !!}
+    {!! Form::text('surname',old('surname'),['class'=>'']) !!}
+    {!! Form::label('email', 'Votre adresse mail') !!}
+    {!! Form::text('email','',['class'=>'']) !!}
+    {!! Form::label('password', 'Votre mot de passe') !!}
+    {!! Form::text('password','',['class'=>'']) !!}
+</fieldset>
+<fieldset>
+    <legend>Les cordonnées de l’institition</legend>
+    {!! Form::label('institution_type','Le type de l’institition') !!}
+    {!! Form::select('institution_type',['asbl'=>'A.S.B.L','ecole_maternelle'=>'École maternelle','ecole_secondaire'=>'École seconfaire'],null,['class'=>'']) !!}
+    {!! Form::label('institution_name', 'Le nom complet de votre école') !!}
+    {!! Form::text('institution_name','',['class'=>'']) !!}
+</fieldset>
+{!! Form::submit('Enregistrer Votre ecole',['class'=>'']) !!}
+{!! Form::close() !!}
