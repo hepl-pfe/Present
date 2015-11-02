7bebb6fa599ddaf0adec0417c49b372ef3bfd33b
Daniel <daniel.schreurs@hotmail.com>
2015-11-02 10:57:55 +0100

    init login

---
+++ beta/1.0.0/resources/views/forms/login.blade.php
@@ -0,0 +1,14 @@
+{!! Form::open(['url' => '/']) !!}
+<fieldset>
+    <legend>Vos coordonnées</legend>
+   mmlklk
+</fieldset>
+<fieldset>
+    <legend>Les cordonnées de l’institition</legend>
+    {!! Form::label('institution_type','Le type de l’institition') !!}
+    {!! Form::select('institution_type',['asbl'=>'A.S.B.L','ecole_maternelle'=>'École maternelle','ecole_secondaire'=>'École seconfaire'],null,['class'=>'']) !!}
+    {!! Form::label('institution_name', 'Le nom complet de votre école') !!}
+    {!! Form::text('institution_name','',['class'=>'']) !!}
+</fieldset>
+{!! Form::submit('Enregistrer Votre ecole',['class'=>'']) !!}
+{!! Form::close() !!}
