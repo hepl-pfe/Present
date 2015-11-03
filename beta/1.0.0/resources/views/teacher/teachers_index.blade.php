41c14abcd2a0abc4146b531d35083455111658c6
Daniel <daniel.schreurs@hotmail.com>
2015-11-03 12:38:57 +0100

    add colleagues file

---
+++ beta/1.0.0/resources/views/teacher/teachers_index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.teacher_layout')
+@section('teacher_content')
+<h1>Collègues</h1>
+<ul>
+    <li>{!! link_to('/teachers/blisntin-stephan','Blisntin Stéphan',['class'=>'','title'=>'Blisntin Stéphan']) !!}</li>
+    <li>{!! link_to('/teachers/blisntin-stephan','Armound Adeline',['class'=>'','title'=>'Blisntin Stéphan']) !!}</li>
+    <li>{!! link_to('/teachers/blisntin-stephan','Crutzen Marie',['class'=>'','title'=>'Blisntin Stéphan']) !!}</li>
+    <li>{!! link_to('/teachers/blisntin-stephan','Delayen Estelle',['class'=>'','title'=>'Blisntin Stéphan']) !!}</li>
+    <li>{!! link_to('/teachers/blisntin-stephan','Chateux Claude',['class'=>'','title'=>'Blisntin Stéphan']) !!}</li>
+    <li>{!! link_to('/teachers/blisntin-stephan','Thury Michel',['class'=>'','title'=>'Blisntin Stéphan']) !!}</li>
+    <li>{!! link_to('/teachers/blisntin-stephan','Mannekin Jan',['class'=>'','title'=>'Blisntin Stéphan']) !!}</li>
+    <li>{!! link_to('/teachers/blisntin-stephan','Blistin Stéphane',['class'=>'','title'=>'Blisntin Stéphan']) !!}</li>
+</ul>
+@stop
