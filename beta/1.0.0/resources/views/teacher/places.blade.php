0cf5f80c9a4ef845035ab42c171a24f1d06ffce9
Daniel <daniel.schreurs@hotmail.com>
2015-11-03 12:38:39 +0100

    add places file

---
+++ beta/1.0.0/resources/views/teacher/places.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.teacher_layout')
+@section('teacher_content')
+<h1>Les locaux</h1>
+<ul>
+    <li>08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li>08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li>08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li>08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li>08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li>08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li>08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li>08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li>08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li>08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li>08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li>08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li>08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+</ul>
+@stop
