334d73c2aeabdfbecbf1da734d7421bb4d6e86e1
Daniel <daniel.schreurs@hotmail.com>
2015-11-03 12:37:01 +0100

    add config file

---
+++ beta/1.0.0/resources/views/teacher/config.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.teacher_layout')
+@section('teacher_content')
+    <h1>Stephan Blisntin</h1>
+    <div>
+        <img src="" alt="" class="media__img">
+        <dl>
+            <dt>mail :</dt>
+            <dd>{!! '1@hotmail.com' !!}</dd>
+            <dt>mail2 :</dt>
+            <dd>{!! '1@hotmail.com' !!}</dd>
+            <dt>Dropbox</dt>
+            <dd>unLien.com</dd>
+        </dl>
+        {!! Form::open(['/']) !!}
+            {!! Form::label('link_name','Nom du lien',['class'=>'']) !!}
+            {!! Form::input('text','link_name','',['class'=>'']) !!}
+            {!! Form::label('link_value','Valeur du lien',['class'=>'']) !!}
+            {!! Form::input('text','link_value','',['class'=>'']) !!}
+            {!! Form::submit('Ajouter le lien',['class'=>'']) !!}
+        {!! Form::close() !!}
+    </div>
+@stop
