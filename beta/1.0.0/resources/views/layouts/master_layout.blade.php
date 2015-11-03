56626f71396956964b2bb5f8e1465cff196764d1
Daniel <daniel.schreurs@hotmail.com>
2015-11-03 15:55:19 +0100

    prepare HTML style

--- beta/1.0.0/resources/views/layouts/master_layout.blade.php
+++ beta/1.0.0/resources/views/layouts/master_layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,100,300,900,700,400italic,100italic' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Présent - @yield('title')</title>
 </head>
