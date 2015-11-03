58828a93050b18bcafe1ee7a4125539b25aa1d6e
Daniel <daniel.schreurs@hotmail.com>
2015-11-03 12:43:50 +0100

    add teacher file

---
+++ beta/1.0.0/resources/views/teacher/teacher_record.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.teacher_layout')
+@section('teacher_content')
+    <h1>Stephan Blisntin</h1>
+    <div>
+        <img src="" alt="" class="media__img">
+        <dl>
+            <dt>mail :</dt>
+            <dd>{!! '1@hotmail.com' !!}</dd>
+            <dt>mail2 :</dt>
+            <dd>{!! '1@hotmail.com' !!}</dd>
+            <dt>Dropbox</dt>
+            <dd>unLien.com</dd>
+        </dl>
+    </div>
+    <div>
+        <div>
+            <h2>Les cours de ....</h2>
+            <ul>
+                <li>08:20 cours avec les 2F</li>
+                <li>08:20 cours avec les 2F</li>
+                <li>08:20 cours avec les 2F</li>
+                <li>08:20 cours avec les 2F</li>
+                <li>08:20 cours avec les 2F</li>
+            </ul>
+        </div>
+        <div>
+            <h2>les élèves de ....</h2>
+            <ul>
+                <li>Armound Adeline</li>
+                <li>Blisntin Stéphan</li>
+                <li>Crutzen Marie</li>
+                <li>Delayen Estelle</li>
+                <li>Gérard Sylvain</li>
+            </ul>
+        </div>
+    </div>
+@stop
