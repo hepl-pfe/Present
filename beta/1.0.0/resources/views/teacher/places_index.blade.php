bd14dac447cedda36c24d69df7764f43402c3036
Daniel <daniel.schreurs@hotmail.com>
2015-11-03 12:37:50 +0100

    add locaux

---
+++ beta/1.0.0/resources/views/teacher/places_index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.teacher_layout')
+@section('teacher_content')
+<h1>Les locaux</h1>
+<ul>
+    <li>
+        <h2>{!! link_to('places/b12','B 12') !!}</h2>
+        <ul>
+            <li>08:20 cours avec les 2F</li>
+            <li>09:20 cours avec les 3G</li>
+            <li>08:20 cours avec les 6G</li>
+            <li>08:20 cours avec les 2J</li>
+        </ul>
+    </li>
+    <li>
+        <h2>{!! link_to('places/b12','B 13') !!}</h2>
+        <ul>
+            <li>08:20 cours avec les 2F</li>
+            <li>09:20 cours avec les 3G</li>
+            <li>08:20 cours avec les 6G</li>
+            <li>08:20 cours avec les 2J</li>
+        </ul>
+    </li>
+    <li>
+        <h2>{!! link_to('places/b12','B 14') !!}</h2>
+        <ul>
+            <li>08:20 cours avec les 2F</li>
+            <li>09:20 cours avec les 3G</li>
+            <li>08:20 cours avec les 6G</li>
+            <li>08:20 cours avec les 2J</li>
+        </ul>
+    </li>
+    <li>
+        <h2>{!! link_to('places/b12','B 15') !!}</h2>
+        <ul>
+            <li>08:20 cours avec les 2F</li>
+            <li>09:20 cours avec les 3G</li>
+            <li>08:20 cours avec les 6G</li>
+            <li>08:20 cours avec les 2J</li>
+        </ul>
+    </li>
+</ul>
+@stop
