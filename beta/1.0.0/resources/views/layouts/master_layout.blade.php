750ee4005cb2af544e5b3f910f4523ae7d8d8c4d
Daniel <daniel.schreurs@hotmail.com>
2015-11-03 22:02:14 +0100

    add spritesheet

--- beta/1.0.0/resources/views/layouts/master_layout.blade.php
+++ beta/1.0.0/resources/views/layouts/master_layout.blade.php
@@ -2,11 +2,15 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link href='https://fonts.googleapis.com/css?family=Lato:400,100,300,900,700,400italic,100italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,100,300,900,700,400italic,100italic' rel='stylesheet'
+          type='text/css'>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Présent - @yield('title')</title>
 </head>
 <body>
-    @yield('content')
+@if(file_exists(public_path() . '/svg/symbol/svg/sprite.symbol.svg'))
+    <?php include(public_path() . '/svg/symbol/svg/sprite.symbol.svg'); ?>
+@endif
+@yield('content')
 </body>
 </html>
