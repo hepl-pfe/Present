cf641100eeac18adcc34027f850b7f244e76bd44
Daniel <daniel.schreurs@hotmail.com>
2015-11-03 22:23:29 +0100

    update

--- beta/1.0.0/resources/views/partials/panel/teacher_controller.blade.php
+++ beta/1.0.0/resources/views/partials/panel/teacher_controller.blade.php
@@ -3,7 +3,11 @@
     <a href=""><img src="./img/you.png" alt=""></a>
 </div>
 <div class="layout__item u-10/12 header-item header-item--right">
-@include('forms.search.search')
+    @include('forms.search.search')
     <a href="">Se déconecter</a>
-    <a href="">Notificationssss</a>
+    <a href="">Notificationssss
+        <svg class="styling-class">
+            <use xlink:href="#document6"></use>
+        </svg>
+    </a>
 </div>
