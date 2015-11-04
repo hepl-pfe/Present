6b855018e16a74dd8ff45e01144e63fa32c0705b
Daniel <daniel.schreurs@hotmail.com>
2015-11-04 17:12:45 +0100

    add layout to the box

--- beta/1.0.0/resources/views/teacher/dashboard.blade.php
+++ beta/1.0.0/resources/views/teacher/dashboard.blade.php
@@ -1,52 +1,54 @@
 @extends('layouts.teacher_layout')
 @section('teacher_content')
-<h1>Paneau de controle</h1>
-<ul>
-    <li>
-        <h2>Mes cours d‘aujourd'hui</h2>
-        <ul>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-        </ul>
-    </li>
-    <li>
-        <h2>Mes élèves</h2>
-        <ul>
-            <li>Armound Adeline</li>
-            <li>Blisntin Stéphan</li>
-            <li>Crutzen Marie</li>
-            <li>Delayen Estelle</li>
-            <li>Gérard Sylvain</li>
-        </ul>
-    </li>
-    <li>
-        <h2>Mes collègues</h2>
-        <ul>
-            <li>Armound Adeline</li>
-            <li>Blisntin Stéphan</li>
-            <li>Crutzen Marie</li>
-            <li>Delayen Estelle</li>
-            <li>Gérard Sylvain</li>
-        </ul>
-    </li>
-    <li>
-        <h2>Mes groupes</h2>
-        <ul>
-            <li>Armound Adeline</li>
-            <li>Blisntin Stéphan</li>
-            <li>Crutzen Marie</li>
-            <li>Delayen Estelle</li>
-            <li>Gérard Sylvain</li>
-        </ul>
-    </li>
-    <li>
-        <h2>Mon compte</h2>
-        <p>Didier Lionel</p>
-        <p>15 heures de cours</p>
-        <p>200 élèves</p>
-    </li>
-</ul>
+<div class="">
+    <h1 class="visuallyhidden">Paneau de controleskjksjksjksj</h1>
+    <ul class="layout">
+        <li class="layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
+            <h2> Mes cours d‘aujourd'hui</h2>
+            <ul>
+                <li>08:20 cours avec les 2F</li>
+                <li>08:20 cours avec les 2F</li>
+                <li>08:20 cours avec les 2F</li>
+                <li>08:20 cours avec les 2F</li>
+                <li>08:20 cours avec les 2F</li>
+            </ul>
+        </li>
+        <li class="layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
+            <h2>Mes élèves</h2>
+            <ul>
+                <li>Armound Adeline</li>
+                <li>Blisntin Stéphan</li>
+                <li>Crutzen Marie</li>
+                <li>Delayen Estelle</li>
+                <li>Gérard Sylvain</li>
+            </ul>
+        </li>
+        <li class="layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
+            <h2>Mes collègues</h2>
+            <ul>
+                <li>Armound Adeline</li>
+                <li>Blisntin Stéphan</li>
+                <li>Crutzen Marie</li>
+                <li>Delayen Estelle</li>
+                <li>Gérard Sylvain</li>
+            </ul>
+        </li>
+        <li class="layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
+            <h2>Mes groupes</h2>
+            <ul>
+                <li>Armound Adeline</li>
+                <li>Blisntin Stéphan</li>
+                <li>Crutzen Marie</li>
+                <li>Delayen Estelle</li>
+                <li>Gérard Sylvain</li>
+            </ul>
+        </li>
+        <li class="layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
+            <h2>Mon compte</h2>
+            <p>Didier Lionel</p>
+            <p>15 heures de cours</p>
+            <p>200 élèves</p>
+        </li>
+    </ul>
+</div>
 @stop
