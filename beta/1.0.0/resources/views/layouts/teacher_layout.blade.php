b5812576071bbcd1197f75deb8ebba0ba326055b
Daniel <daniel.schreurs@hotmail.com>
2015-11-04 15:03:21 +0100

    init main nav

--- beta/1.0.0/resources/views/layouts/teacher_layout.blade.php
+++ beta/1.0.0/resources/views/layouts/teacher_layout.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master_layout')
 @section('content')
-<div role="header" class="layout">
+<div role="header" class="layout main-header">
     @include('partials.panel.teacher_controller')
 </div>
 <main class="layout">
--- beta/1.0.0/resources/views/partials/nav/teacher_nav.blade.php
+++ beta/1.0.0/resources/views/partials/nav/teacher_nav.blade.php
@@ -1,8 +1,10 @@
-<div role="navigation" class="layout__item u-2/12-desk u-6/12-lap u-12/12-palm">
-    {!! link_to('/cours','Mes cours',['class'=>'','title'=>'Renvoie vers le paneaux de controle des cours']) !!}
-    {!! link_to('/eleves','Mes Élèves',['class'=>'','title'=>'Renvoie vers le paneaux de controle des élèves']) !!}
-    {!! link_to('/groups','Mes groupes',['class'=>'','title'=>'Renvoie vers le paneaux de controle des groupes']) !!}
-    {!! link_to('/teachers','Collègues',['class'=>'','title'=>'Renvoie vers le paneaux de controle des collègues']) !!}
-    {!! link_to('/places','Locaux',['class'=>'','title'=>'Renvoie vers le paneaux de controle des locaux']) !!}
-    {!! link_to('/teacher/config/blisntin-stephan','Mon compte',['class'=>'','title'=>'Renvoie vers le paneaux de controle de vore comptsssse']) !!}
+<div role="navigation" class="layout__item u-2/12-desk u-6/12-lap u-12/12-palm main-nav">
+    <ul class="list-block">
+        <li class="list-block__item">{!! link_to('/cours','Mes cours',['class'=>'','title'=>'Renvoie vers le paneaux de controle des cours']) !!}</li>
+        <li class="list-block__item">{!! link_to('/eleves','Mes Élèves',['class'=>'','title'=>'Renvoie vers le paneaux de controle des élèves']) !!}</li>
+        <li class="list-block__item">{!! link_to('/groups','Mes groupes',['class'=>'','title'=>'Renvoie vers le paneaux de controle des groupes']) !!}</li>
+        <li class="list-block__item">{!! link_to('/teachers','Collègues',['class'=>'','title'=>'Renvoie vers le paneaux de controle des collègues']) !!}</li>
+        <li class="list-block__item">{!! link_to('/places','Locaux',['class'=>'','title'=>'Renvoie vers le paneaux de controle des locaux']) !!}</li>
+        <li class="list-block__item">{!! link_to('/teacher/config/blisntin-stephan','Mon compte',['class'=>'','title'=>'Renvoie vers le paneaux de controle de vore comptsssse']) !!}</li>
+    </ul>
 </div>
