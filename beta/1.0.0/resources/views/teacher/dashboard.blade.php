ecbdabb5a6f6bb471386a29de3338e7c17518d96
Daniel <daniel.schreurs@hotmail.com>
2015-11-04 18:21:01 +0100

    add box item

--- beta/1.0.0/resources/views/teacher/dashboard.blade.php
+++ beta/1.0.0/resources/views/teacher/dashboard.blade.php
@@ -1,54 +1,54 @@
 @extends('layouts.teacher_layout')
 @section('teacher_content')
-<div class="">
-    <h1 class="visuallyhidden">Paneau de controleskjksjksjksj</h1>
-    <ul class="layout">
-        <li class="layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
+<h1 class="visuallyhidden">Paneau de controleskjksjksjksj</h1>
+<ul class="layout">
+    <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
             <h2> Mes cours d‘aujourd'hui</h2>
-            <ul>
+            <ul class="box">
                 <li>08:20 cours avec les 2F</li>
                 <li>08:20 cours avec les 2F</li>
                 <li>08:20 cours avec les 2F</li>
                 <li>08:20 cours avec les 2F</li>
                 <li>08:20 cours avec les 2F</li>
             </ul>
-        </li>
-        <li class="layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
+    </li>
+    <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
             <h2>Mes élèves</h2>
-            <ul>
+            <ul class="box">
                 <li>Armound Adeline</li>
                 <li>Blisntin Stéphan</li>
                 <li>Crutzen Marie</li>
                 <li>Delayen Estelle</li>
                 <li>Gérard Sylvain</li>
             </ul>
-        </li>
-        <li class="layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
+    </li>
+    <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
             <h2>Mes collègues</h2>
-            <ul>
+            <ul class="box">
                 <li>Armound Adeline</li>
                 <li>Blisntin Stéphan</li>
                 <li>Crutzen Marie</li>
                 <li>Delayen Estelle</li>
                 <li>Gérard Sylvain</li>
             </ul>
-        </li>
-        <li class="layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
+    </li>
+    <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
             <h2>Mes groupes</h2>
-            <ul>
+            <ul class="box">
                 <li>Armound Adeline</li>
                 <li>Blisntin Stéphan</li>
                 <li>Crutzen Marie</li>
                 <li>Delayen Estelle</li>
                 <li>Gérard Sylvain</li>
             </ul>
-        </li>
-        <li class="layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
-            <h2>Mon compte</h2>
+    </li>
+    <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
+        <h2>Mon compte</h2>
+        <div class="box">
             <p>Didier Lionel</p>
             <p>15 heures de cours</p>
             <p>200 élèves</p>
-        </li>
-    </ul>
-</div>
+        </div>
+    </li>
+</ul>
 @stop
