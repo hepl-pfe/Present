425d784502b95ed6072e2bba9c7a60f88d35580d
Daniel <daniel.schreurs@hotmail.com>
2015-11-05 21:35:04 +0100

    update droplet

--- beta/1.0.0/resources/views/partials/panel/teacher_controller.blade.php
+++ beta/1.0.0/resources/views/partials/panel/teacher_controller.blade.php
@@ -2,12 +2,20 @@
     <div class="layout__item u-8/12">
         @include('forms.search.search')
     </div>
-    <a href=""  class="layout__item u-1/12">
-        <svg class="svg-basic">
-            <title>Notifications</title>
-            <use xlink:href="#notifications"></use>
-        </svg>
-    </a>
+    <div class="layout__item u-1/12">
+        <div class="dropdown-menu-container dropdown-menu-container--hover">
+            <svg class="svg-basic">
+                <title>Notifications</title>
+                <use xlink:href="#notifications"></use>
+            </svg>
+            <div class="dropdown-menu">
+                <ul>
+                    <li class="dropdown-menu__item">Blabla</li>
+                    <li class="dropdown-menu__item"><a href="">Configuration</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
     <div class="layout__item u-3/12">
        <div class="media dropdown-menu-container dropdown-menu-container--hover">
            <img src="{!! asset('./img/default_profile_picture.jpg') !!}" alt="" class="media__img user-image user-image--small">
