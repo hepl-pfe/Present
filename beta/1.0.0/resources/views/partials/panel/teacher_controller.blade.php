40cbcd3aee45884d974b89a34b857e9d3f93720d
Daniel <daniel.schreurs@hotmail.com>
2015-11-05 20:47:44 +0100

    update header

--- beta/1.0.0/resources/views/partials/panel/teacher_controller.blade.php
+++ beta/1.0.0/resources/views/partials/panel/teacher_controller.blade.php
@@ -1,25 +1,26 @@
-<div class="">
-    <div class="2">
+<div class="layout">
+    <div class="layout__item u-8/12">
         @include('forms.search.search')
     </div>
-    <div class="">
-        <a href="">
-            <svg class="svg-basic">
-                <title>Se déconecter</title>
-                <use xlink:href="#power"></use>
-            </svg>
-        </a>
-        <a href="">
-            <svg class="svg-basic">
-                <title>Notifications</title>
-                <use xlink:href="#notifications"></use>
-            </svg>
-        </a>
-        <div class="">
-            <a class="" href="{!! URL::to('/')!!}">
-                <img src="{!! asset('img/default_profile_picture.jpg') !!}" alt="" class="user-image--small user-image">
-            </a>
-            <span class="">Votre nom</span>
-        </div>
+    <a href=""  class="layout__item u-1/12">
+        <svg class="svg-basic">
+            <title>Notifications</title>
+            <use xlink:href="#notifications"></use>
+        </svg>
+    </a>
+    <div class="layout__item u-3/12">
+       <div class="media">
+           <img src="{!! asset('./img/default_profile_picture.jpg') !!}" alt="" class="media__img user-image user-image--small">
+           <a href="" class="media__body">
+               <span>Votre nom et prénom </span>
+               <span>Votre fonction</span>
+           </a>
+           <div class="dropdown-menu">
+               <ul>
+                   <li>Blabla</li>
+                   <li><a href="">Configuration</a></li>
+               </ul>
+           </div>
+       </div>
     </div>
 </div>
