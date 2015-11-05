de1b9bda9cda5853f3abb6dbda4e5f4ca3fa9624
Daniel <daniel.schreurs@hotmail.com>
2015-11-05 16:32:33 +0100

    add header style

--- beta/1.0.0/resources/views/teacher/teacher_record.blade.php
+++ beta/1.0.0/resources/views/teacher/teacher_record.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.teacher_layout')
 @section('teacher_content')
-    <h1>Stephan Blisntin</h1>
-    <div>
-        <img src="" alt="" class="media__img">
-        <dl>
+    <h1 class="big-page-header">Stephan Blisntin</h1>
+    <div class="media section">
+        <img src="{!! asset('img/default_profile_picture.jpg') !!}" alt="" class="media__img user-image user-image--medium">
+        <dl class="media-body">
             <dt>mail :</dt>
             <dd>{!! '1@hotmail.com' !!}</dd>
             <dt>mail2 :</dt>
@@ -12,26 +12,26 @@
             <dd>unLien.com</dd>
         </dl>
     </div>
-    <div>
-        <div>
-            <h2>Les cours de ....</h2>
-            <ul>
+    <ul class="layout">
+        <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
+            <h2 class="bow-header box-header--big">Les cours de Stephan Blisntin d‘aujourd'hui</h2>
+            <ul class="box">
                 <li>08:20 cours avec les 2F</li>
                 <li>08:20 cours avec les 2F</li>
                 <li>08:20 cours avec les 2F</li>
                 <li>08:20 cours avec les 2F</li>
                 <li>08:20 cours avec les 2F</li>
             </ul>
-        </div>
-        <div>
-            <h2>les élèves de ....</h2>
-            <ul>
+        </li>
+        <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
+            <h2 class="bow-header box-header--big">Les élèves de Stephan Blisntin</h2>
+            <ul class="box">
                 <li>Armound Adeline</li>
                 <li>Blisntin Stéphan</li>
                 <li>Crutzen Marie</li>
                 <li>Delayen Estelle</li>
                 <li>Gérard Sylvain</li>
             </ul>
-        </div>
-    </div>
+        </li>
+    </ul>
 @stop
