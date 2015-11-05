0f631cf5ba6900bcd24a253a9bf8d73567b7b5af
Daniel <daniel.schreurs@hotmail.com>
2015-11-05 18:37:24 +0100

    update colors

--- beta/1.0.0/resources/views/partials/nav/teacher_nav.blade.php
+++ beta/1.0.0/resources/views/partials/nav/teacher_nav.blade.php
@@ -1,59 +1,59 @@
 <div role="navigation" class="layout__item u-2/12-desk u-2/12-lap u-12/12-palm main-nav">
     <ul class="list-block">
         <li class="list-block__item">
-            <a href="{!! URL::to('/') !!}" class="media">
+            <a href="{!! URL::to('/') !!}" class="media no-underline">
                 <svg class="svg-basic svg-nav media__img media__img--svg">
                     <use xlink:href="#home"></use>
                 </svg>
-                <span class="media-body delta">Paneau de contrôle</span>
+                <span class="media-body delta navbar-item">Paneau de contrôle</span>
             </a>
         </li>
         <li class="list-block__item">
-            <a href="{!! URL::to('/cours') !!}" class="media">
+            <a href="{!! URL::to('/cours') !!}" class="media no-underline">
                 <svg class="svg-basic svg-nav media__img media__img--svg">
                     <use xlink:href="#cours"></use>
                 </svg>
-                <span class="media-body delta">Mes cours</span>
+                <span class="media-body delta navbar-item">Mes cours</span>
             </a>
         </li>
         <li class="list-block__item">
-            <a href="{!! URL::to('/eleves') !!}" class="media">
+            <a href="{!! URL::to('/eleves') !!}" class="media no-underline">
                 <svg class="svg-basic svg-nav media__img media__img--svg">
                     <use xlink:href="#power"></use>
                 </svg>
-                <span class="media-body delta">Mes Élèves</span>
+                <span class="media-body delta navbar-item">Mes Élèves</span>
             </a>
         </li>
         <li class="list-block__item">
-            <a href="{!! URL::to('/groups') !!}" class="media">
+            <a href="{!! URL::to('/groups') !!}" class="media no-underline">
                 <svg class="svg-basic svg-nav media__img media__img--svg">
                     <use xlink:href="#classes"></use>
                 </svg>
-                <span class="media-body delta">Mes groupes</span>
+                <span class="media-body delta navbar-item">Mes groupes</span>
             </a>
         </li>
         <li class="list-block__item">
-            <a href="{!! URL::to('/teachers') !!}" class="media">
+            <a href="{!! URL::to('/teachers') !!}" class="media no-underline">
                 <svg class="svg-basic svg-nav media__img media__img--svg">
                     <use xlink:href="#collegues"></use>
                 </svg>
-                <span class="media-body delta">Collègues</span>
+                <span class="media-body delta navbar-item">Collègues</span>
             </a>
         </li>
         <li class="list-block__item">
-            <a href="{!! URL::to('/places') !!}" class="media">
+            <a href="{!! URL::to('/places') !!}" class="media no-underline">
                 <svg class="svg-basic svg-nav media__img media__img--svg">
                     <use xlink:href="#columns"></use>
                 </svg>
-                <span class="media-body delta">Locaux</span>
+                <span class="media-body delta navbar-item">Locaux</span>
             </a>
         </li>
         <li class="list-block__item">
-            <a href="{!! URL::to('/teacher/config/blisntin-stephan') !!}" class="media">
+            <a href="{!! URL::to('/teacher/config/blisntin-stephan') !!}" class="media no-underline">
                 <svg class="svg-basic svg-nav media__img media__img--svg">
                     <use xlink:href="#compte"></use>
                 </svg>
-                <span class="media-body delta">Mon compte</span>
+                <span class="media-body delta navbar-item">Mon compte</span>
             </a>
         </li>
     </ul>
--- beta/1.0.0/resources/views/teacher/dashboard.blade.php
+++ beta/1.0.0/resources/views/teacher/dashboard.blade.php
@@ -3,7 +3,7 @@
 <h1 class="visuallyhidden">Paneau de controleskjksjksjksj</h1>
 <ul class="layout">
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
-            <h2> Mes cours d‘aujourd'hui</h2>
+            <h2 class="box-header"> Mes cours d‘aujourd'hui</h2>
             <ul class="box">
                 <li>08:20 cours avec les 2F</li>
                 <li>08:20 cours avec les 2F</li>
@@ -13,17 +13,7 @@
             </ul>
     </li>
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
-            <h2>Mes élèves</h2>
-            <ul class="box">
-                <li>Armound Adeline</li>
-                <li>Blisntin Stéphan</li>
-                <li>Crutzen Marie</li>
-                <li>Delayen Estelle</li>
-                <li>Gérard Sylvain</li>
-            </ul>
-    </li>
-    <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
-            <h2>Mes collègues</h2>
+            <h2 class="box-header">Mes élèves</h2>
             <ul class="box">
                 <li>Armound Adeline</li>
                 <li>Blisntin Stéphan</li>
@@ -33,7 +23,7 @@
             </ul>
     </li>
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
-            <h2>Mes groupes</h2>
+            <h2 class="box-header">Mes collègues</h2>
             <ul class="box">
                 <li>Armound Adeline</li>
                 <li>Blisntin Stéphan</li>
@@ -43,7 +33,17 @@
             </ul>
     </li>
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
-        <h2>Mon compte</h2>
+            <h2 class="box-header">Mes groupes</h2>
+            <ul class="box">
+                <li>Armound Adeline</li>
+                <li>Blisntin Stéphan</li>
+                <li>Crutzen Marie</li>
+                <li>Delayen Estelle</li>
+                <li>Gérard Sylvain</li>
+            </ul>
+    </li>
+    <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
+        <h2 class="box-header">Mon compte</h2>
         <div class="box">
             <p>Didier Lionel</p>
             <p>15 heures de cours</p>
