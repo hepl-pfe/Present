2a474936e0badb338b63b468ba78bcbeb30a4086
Daniel <daniel.schreurs@hotmail.com>
2015-11-05 16:39:31 +0100

    make it lighter

--- beta/1.0.0/resources/views/teacher/config.blade.php
+++ beta/1.0.0/resources/views/teacher/config.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.teacher_layout')
 @section('teacher_content')
-<h1 big-page-header>Stephan Blisntin</h1>
+<h1 class="big-page-header">Stephan Blisntin</h1>
 <div>
     <img src="" alt="" class="media__img">
     <dl>
--- beta/1.0.0/resources/views/teacher/cours.blade.php
+++ beta/1.0.0/resources/views/teacher/cours.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.teacher_layout')
 @section('teacher_content')
-<h1 class="big-page-header">Cours de...</h1>
+<h1 class="big-page-header">Cours de..</h1>
 <ul>
     <li>Charle</li><li>Charle</li><li>Charle</li><li>Charle</li><li>Charle</li><li>Charle</li>
 </ul>
