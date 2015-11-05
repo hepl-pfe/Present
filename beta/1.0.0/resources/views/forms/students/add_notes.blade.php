211512087287880553507e27002f0dc5d75a7c25
Daniel <daniel.schreurs@hotmail.com>
2015-11-05 15:59:40 +0100

    I love CSS

--- beta/1.0.0/resources/views/forms/students/add_notes.blade.php
+++ beta/1.0.0/resources/views/forms/students/add_notes.blade.php
@@ -2,14 +2,9 @@
     {!! Form::select('secteur_note',['Note relative aux cours'=>'notes_cours','Notes relatives à l’année'],['selected'=>'notes_cours'],['class'=>'simple-select']) !!}
     {!! Form::label('secteur_note','Ajouter une note',['class'=>'simple-label']) !!}
 </div>
-<div class="form-group">
-    {!! Form::textarea('note_text','',['class'=>'input']) !!}
-    {!! Form::label('note_text','',['class'=>'label-paceholder']) !!}
+<div class="form-group form-group--large">
+        {!! Form::textarea('note_text','',['class'=>'input form-group--large__input']) !!}
+        {!! Form::label('note_text','',['class'=>'label-paceholder']) !!}
+        {!! Form::submit('Ajouter la note',['class'=>'form-group--large__submit']) !!}
 </div>
-<div class="form-group">
-    {!! Form::text('node','',['class'=>'input']) !!}
-    {!! Form::label('node','',['class'=>'label-paceholder']) !!}
-</div>
-<div class="form-group">
-    {!! Form::submit('Ajouter la note',['class'=>'']) !!}
-</div>
+
