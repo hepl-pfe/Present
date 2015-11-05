0eb8dfa40f91f18d966ffa70d556405d18090215
Daniel <daniel.schreurs@hotmail.com>
2015-11-05 12:23:27 +0100

    add basic form style

--- beta/1.0.0/resources/views/forms/students/add_notes.blade.php
+++ beta/1.0.0/resources/views/forms/students/add_notes.blade.php
@@ -1,5 +1,12 @@
-{!! Form::label('secteur_note','',['class'=>'']) !!}
-{!! Form::select('secteur_note',['Note relative aux cours'=>'notes_cours','Notes relatives à l’année'],['selected'=>'notes_cours'],['class'=>'']) !!}
-{!! Form::label('note_text','',['class'=>'']) !!}
-{!! Form::textarea('note_text','',['class'=>'']) !!}
-{!! Form::submit('Ajouter la note',['class'=>'']) !!}
+<div class="form-group">
+    {!! Form::label('secteur_note','Ajouter une note',['class'=>'simple-label']) !!}
+    {!! Form::select('secteur_note',['Note relative aux cours'=>'notes_cours','Notes relatives à l’année'],['selected'=>'notes_cours'],['class'=>'simple-select']) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('note_text','',['class'=>'']) !!}
+    {!! Form::textarea('note_text','',['class'=>'']) !!}
+    {!! Form::text('n','',['class'=>'']) !!}
+</div>
+<div class="form-group">
+    {!! Form::submit('Ajouter la note',['class'=>'']) !!}
+</div>
