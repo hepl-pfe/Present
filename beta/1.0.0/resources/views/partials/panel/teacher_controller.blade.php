da431c30bfa8bc187b8c0db3ec2b87eb9edc30a6
Daniel <daniel.schreurs@hotmail.com>
2015-11-06 12:08:29 +0100

    fixe text in header

--- beta/1.0.0/resources/views/partials/panel/teacher_controller.blade.php
+++ beta/1.0.0/resources/views/partials/panel/teacher_controller.blade.php
@@ -1,5 +1,14 @@
 <div class="layout">
-    <div class="layout__item u-8/12">
+    <div class="layout__item u-2/12 logo media">
+        <svg class="svg-basic svg-main-logo media__img--svg media__img">
+            <title>Notifications</title>
+            <use xlink:href="#notifications"></use>
+        </svg>
+        <div class="media__body">
+            <span class="delta logo__item">Présent</span>
+        </div>
+    </div>
+    <div class="layout__item u-7/12">
         @include('forms.search.search')
     </div>
     <div class="layout__item u-1/12">
@@ -17,13 +26,14 @@
 
             <div class="dropdown-menu">
                 <ul>
-                    <li class="dropdown-menu__item">Blabla</li>
-                    <li class="dropdown-menu__item"><a href="">Configuration</a></li>
+                    <li class="dropdown-menu__item">Une news</li>
+                    <li class="dropdown-menu__item"><a href="">Encore une news</a></li>
+                    <li class="dropdown-menu__item"><a href="">Encore une news</a></li>
                 </ul>
             </div>
         </div>
     </div>
-    <div class="layout__item u-3/12">
+    <div class="layout__item u-2/12">
        <div class="media dropdown-menu-container dropdown-menu-container--hover">
            <img src="{!! asset('./img/default_profile_picture.jpg') !!}" alt="" class="media__img user-image user-image--small">
            <a href="" class="media__body header-meta no-underline">
@@ -37,7 +47,15 @@
            <div class="dropdown-menu">
                <ul>
                    <li class="dropdown-menu__item">Blabla</li>
-                   <li class="dropdown-menu__item"><a href="">Configuration</a></li>
+                   <li class="dropdown-menu__item">
+                       <a href="">
+                           <svg class="svg-basic dropdown-menu__arraw">
+                               <title>Configuration</title>
+                               <use xlink:href="#compte"></use>
+                           </svg>
+                           Configurationsss
+                       </a>
+                   </li>
                </ul>
            </div>
        </div>
