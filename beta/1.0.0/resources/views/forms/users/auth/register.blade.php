bf491cbda2262f71ecf66ad4429ec7d68ea8e4b7
Daniel <daniel.schreurs@hotmail.com>
2015-11-08 15:45:40 +0100

    update form style

--- beta/1.0.0/resources/views/forms/users/auth/register.blade.php
+++ beta/1.0.0/resources/views/forms/users/auth/register.blade.php
@@ -1,29 +1,22 @@
 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 <div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
-        {!! Form::label('name','Votre prénom',['class'=>'floating-placeholder--label floating-placeholder--label--blue']) !!}
-        <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+    {!! Form::label('name','Votre prénom',['class'=>'floating-placeholder__label floating-placeholder__label--blue']) !!}
+    {!! Form::input('text','name',old('name'),['class'=>'floating-placeholder__input--huge floating-placeholder__input']) !!}
 </div>
 
 <div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
-    {!! Form::label('email','Votre adresse email',['class'=>'floating-placeholder--label floating-placeholder--label--blue']) !!}
-        <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+    {!! Form::label('email','Votre adresse email',['class'=>'floating-placeholder__label floating-placeholder__label--blue']) !!}
+    {!! Form::input('email','email',old('email'),['class'=>'floating-placeholder__input--huge floating-placeholder__input']) !!}
 </div>
 
 <div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
-    <label class="">Password</label>
-    <input type="password" class="form-control" name="password">
+    {!! Form::label('password','Votre mot de passe',['class'=>'floating-placeholder__label floating-placeholder__label--blue']) !!}
+    {!! Form::input('password','password',old('email'),['class'=>'floating-placeholder__input--huge floating-placeholder__input']) !!}
 </div>
 
 <div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
-    <label class="">Confirm Password</label>
-        <input type="password" class="form-control" name="password_confirmation">
+    {!! Form::label('password_confirmation','Confirmez votre mot de passe',['class'=>'floating-placeholder__label floating-placeholder__label--blue']) !!}
+    {!! Form::input('password','password_confirmation',old('email'),['class'=>'floating-placeholder__input--huge floating-placeholder__input']) !!}
 </div>
-
-<div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
-    <div class="col-md-6 col-md-offset-4">
-        <button type="submit" class="btn btn-primary">
-            Register
-        </button>
-    </div>
-</div>
+{!! Form::submit('S’enregister') !!}
