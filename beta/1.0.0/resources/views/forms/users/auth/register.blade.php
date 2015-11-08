e97b6a067d7e6a7807e88944feb106bb15e55d98
Daniel <daniel.schreurs@hotmail.com>
2015-11-08 14:59:51 +0100

    update register

---
+++ beta/1.0.0/resources/views/forms/users/auth/register.blade.php
@@ -0,0 +1,29 @@
+<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+<div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
+    <label class="">Name</label>
+        <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+</div>
+
+<div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
+    <label class="">E-Mail Address</label>
+        <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+</div>
+
+<div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
+    <label class="">Password</label>
+    <input type="password" class="form-control" name="password">
+</div>
+
+<div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
+    <label class="">Confirm Password</label>
+        <input type="password" class="form-control" name="password_confirmation">
+</div>
+
+<div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
+    <div class="col-md-6 col-md-offset-4">
+        <button type="submit" class="btn btn-primary">
+            Register
+        </button>
+    </div>
+</div>
