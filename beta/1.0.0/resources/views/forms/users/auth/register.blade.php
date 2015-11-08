be9e78d432017be406a7906b0033c8ce93538674
Daniel <daniel.schreurs@hotmail.com>
2015-11-08 16:04:29 +0100

    update btn

--- beta/1.0.0/resources/views/forms/users/auth/register.blade.php
+++ beta/1.0.0/resources/views/forms/users/auth/register.blade.php
@@ -1,5 +1,3 @@
-<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
 <div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
     {!! Form::label('name','Votre prénom',['class'=>'floating-placeholder__label floating-placeholder__label--blue']) !!}
     {!! Form::input('text','name',old('name'),['class'=>'floating-placeholder__input--huge floating-placeholder__input']) !!}
@@ -19,4 +17,4 @@
     {!! Form::label('password_confirmation','Confirmez votre mot de passe',['class'=>'floating-placeholder__label floating-placeholder__label--blue']) !!}
     {!! Form::input('password','password_confirmation',old('email'),['class'=>'floating-placeholder__input--huge floating-placeholder__input']) !!}
 </div>
-{!! Form::submit('S’enregister',['class','btn']) !!}
+{!! Form::submit('S’enregister',['class','btn btn--large']) !!}
