afa61e56daa54d340fdeb2f7f6f6184c40e19915
Daniel <daniel.schreurs@hotmail.com>
2015-11-09 22:20:33 +0100

    add links

--- beta/1.0.0/resources/views/teacher/cours_index.blade.php
+++ beta/1.0.0/resources/views/teacher/cours_index.blade.php
@@ -1,35 +1,36 @@
 @extends('layouts.teacher_layout')
 @section('teacher_content')
 <h1 class="big-page-header">Mes cours</h1>
+{!! Html::link('/ajouter/cours','Ajouter un cours',['class'=>'btn']) !!}
 <ul class="layout">
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
         <h2 class="box-header">{!! link_to('cours/français','Français',['class'=>'']) !!}</h2>
         <ul class="box">
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
+            <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+            <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+            <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+            <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+            <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
         </ul>
     </li>
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
         <h2 class="box-header">{!! link_to('cours/français','Théatre',['class'=>'']) !!}</h2>
         <ul class="box">
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
+            <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+            <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+            <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+            <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+            <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
         </ul>
     </li>
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
         <h2 class="box-header">{!! link_to('cours/français','Expression écrite',['class'=>'']) !!}</h2>
         <ul class="box">
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
-            <li>08:20 cours avec les 2F</li>
+            <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+            <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+            <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+            <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+            <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
         </ul>
     </li>
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
