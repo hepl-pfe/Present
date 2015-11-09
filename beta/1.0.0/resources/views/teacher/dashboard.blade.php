6da250c4ba3d9c391bd9adbdfa068f3446377aa9
Daniel <daniel.schreurs@hotmail.com>
2015-11-09 22:08:20 +0100

    add link

--- beta/1.0.0/resources/views/teacher/dashboard.blade.php
+++ beta/1.0.0/resources/views/teacher/dashboard.blade.php
@@ -5,41 +5,41 @@
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
             <h2 class="box-header"> Mes cours d‘aujourd'hui</h2>
             <ul class="box">
-                <li>08:20 cours avec les 2F</li>
-                <li>08:20 cours avec les 2F</li>
-                <li>08:20 cours avec les 2F</li>
-                <li>08:20 cours avec les 2F</li>
-                <li>08:20 cours avec les 2F</li>
+                <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+                <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+                <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+                <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
+                <li>08:20 cours avec les {!! Html::link('/cours/français','2F') !!}</li>
             </ul>
     </li>
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
             <h2 class="box-header">Mes élèves</h2>
             <ul class="box">
-                <li>Armound Adeline</li>
-                <li>Blisntin Stéphan</li>
-                <li>Crutzen Marie</li>
-                <li>Delayen Estelle</li>
-                <li>Gérard Sylvain</li>
+                <li>{!! Html::link('/eleves/blisntin-stephan','Armound Adeline') !!}</li>
+                <li>{!! Html::link('/eleves/blisntin-stephan','Blisntin Stéphan') !!}</li>
+                <li>{!! Html::link('/eleves/blisntin-stephan','Crutzen Marie') !!}</li>
+                <li>{!! Html::link('/eleves/blisntin-stephan','Delayen Estelle') !!}</li>
+                <li>{!! Html::link('/eleves/blisntin-stephan','Gérard Sylvain') !!}</li>
             </ul>
     </li>
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
             <h2 class="box-header">Mes collègues</h2>
             <ul class="box">
-                <li>Armound Adeline</li>
-                <li>Blisntin Stéphan</li>
-                <li>Crutzen Marie</li>
-                <li>Delayen Estelle</li>
-                <li>Gérard Sylvain</li>
+                <li>{!! Html::link('/teachers/blisntin-stephan','Armound Adeline') !!}</li>
+                <li>{!! Html::link('/teachers/blisntin-stephan','Blisntin Stéphan') !!}</li>
+                <li>{!! Html::link('/teachers/blisntin-stephan','Crutzen Marie') !!}</li>
+                <li>{!! Html::link('/teachers/blisntin-stephan','Delayen Estelle') !!}</li>
+                <li>{!! Html::link('/teachers/blisntin-stephan','Gérard Sylvain') !!}</li>
             </ul>
     </li>
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
             <h2 class="box-header">Mes groupes</h2>
             <ul class="box">
-                <li>Armound Adeline</li>
-                <li>Blisntin Stéphan</li>
-                <li>Crutzen Marie</li>
-                <li>Delayen Estelle</li>
-                <li>Gérard Sylvain</li>
+                <li>{!! Html::link('/eleves/blisntin-stephan','Armound Adeline') !!}</li>
+                <li>{!! Html::link('/eleves/blisntin-stephan','Blisntin Stéphan') !!}</li>
+                <li>{!! Html::link('/eleves/blisntin-stephan','Crutzen Marie') !!}</li>
+                <li>{!! Html::link('/eleves/blisntin-stephan','Delayen Estelle') !!}</li>
+                <li>{!! Html::link('/eleves/blisntin-stephan','Gérard Sylvain') !!}</li>
             </ul>
     </li>
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
