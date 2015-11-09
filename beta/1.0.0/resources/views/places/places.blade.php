a1cb79ed077215385ba30a89e3de22a1b1d7e5d5
Daniel <daniel.schreurs@hotmail.com>
2015-11-09 18:03:02 +0100

    add classe

---
+++ beta/1.0.0/resources/views/places/places.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.teacher_layout')
+@section('teacher_content')
+<h1 class="big-page-header">Les locaux</h1>
+<ul class="places-box list-block list-block--small">
+    <li class="list-block__item--small places__item">08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li class="list-block__item--small places__item">08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li class="list-block__item--small places__item">08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li class="list-block__item--small places__item">08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li class="list-block__item--small places__item">08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li class="list-block__item--small places__item">08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li class="list-block__item--small places__item">08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li class="list-block__item--small places__item">08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li class="list-block__item--small places__item">08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li class="list-block__item--small places__item">08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li class="list-block__item--small places__item">08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li class="list-block__item--small places__item">08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+    <li class="list-block__item--small places__item">08:20 cours {!! link_to('/cours/français','Français') !!} avec  {!! link_to('/teachers/blisntin-stephan','Blisntin-Stephan') !!} avec les 2F</li>
+</ul>
+@stop
