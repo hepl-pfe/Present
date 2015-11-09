0cdc3a1ba9f2ff9bf161ab96df950df3c37bb78a
Daniel <daniel.schreurs@hotmail.com>
2015-11-09 13:25:50 +0100

    add middlewares

--- beta/1.0.0/app/Http/routes.php
+++ beta/1.0.0/app/Http/routes.php
@@ -11,37 +11,37 @@
     |
     */
 
-    Route::get('/', function () {
+    Route::get('/',['middleware' => 'auth'], function () {
         return view('teacher.dashboard');
     });
-    Route::get('/cours', function () {
+    Route::get('/cours',['middleware' => 'auth'], function () {
         return view('teacher.cours_index');
     });
-    Route::get('/cours/français', function () {
+    Route::get('/cours/français',['middleware' => 'auth'], function () {
         return view('teacher.cours');
     });
-    Route::get('/eleves', function () {
+    Route::get('/eleves',['middleware' => 'auth'], function () {
         return view('teacher.students_index');
     });
-    Route::get('/eleves/blisntin-stephan', function () {
+    Route::get('/eleves/blisntin-stephan',['middleware' => 'auth'], function () {
         return view('teacher.student_record');
     });
-    Route::get('/teachers', function () {
+    Route::get('/teachers',['middleware' => 'auth'], function () {
         return view('teacher.teachers_index');
     });
-    Route::get('/teachers/blisntin-stephan', function () {
+    Route::get('/teachers/blisntin-stephan',['middleware' => 'auth'], function () {
         return view('teacher.teacher_record');
     });
-    Route::get('/groups', function () {
+    Route::get('/groups',['middleware' => 'auth'], function () {
         return view('teacher.groups_index');
     });
-    Route::get('/places', function () {
+    Route::get('/places',['middleware' => 'auth'], function () {
         return view('teacher.places_index');
     });
-    Route::get('places/b12', function () {
+    Route::get('places/b12',['middleware' => 'auth'], function () {
         return view('teacher.places');
     });
-    Route::get('teacher/config/blisntin-stephan', function () {
+    Route::get('teacher/config/blisntin-stephan',['middleware' => 'auth'], function () {
         return view('teacher.config');
     });
     Route::controllers([
