c3cf876a3a70982a08125020f62b5ea1d8d3735c
Daniel <daniel.schreurs@hotmail.com>
2015-11-11 16:22:54 +0100

    add relation

---
+++ beta/1.0.0/database/migrations/2015_11_11_152041_ad_relation_table_for_user.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AdRelationTableForUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('student_id')->unsigned();
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('student_user',function(Blueprint $table){
+            $table->dropForeign('user_student_id_foreign');
+        });
+
+    }
+}
