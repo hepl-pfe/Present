c0e8d819e1b202008d4b8033ba50525d315ae66d
Daniel <daniel.schreurs@hotmail.com>
2015-11-13 20:22:39 +0100

    add route

--- beta/1.0.0/app/Http/Routes/Api/ApiRoutes.php
+++ beta/1.0.0/app/Http/Routes/Api/ApiRoutes.php
@@ -1,2 +1,9 @@
 <?php
     Route::resource('user','Api\UserController');
+    Route::resource('student','Api\StudentController');
+    Route::get('/',function(){
+        $routeCollection = Route::getRoutes();
+        foreach ($routeCollection as $route) {
+            echo($route->getPath().'</br>');
+    }
+    });
