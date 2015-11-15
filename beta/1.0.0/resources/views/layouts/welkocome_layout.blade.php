262e6b7c2c7a538618d9cc03681457e5f21ca62e
Daniel <daniel.schreurs@hotmail.com>
2015-11-15 22:24:12 +0100

    fixe unused forms

--- beta/1.0.0/resources/views/layouts/welkocome_layout.blade.php
+++ beta/1.0.0/resources/views/layouts/welkocome_layout.blade.php
@@ -1,8 +1,5 @@
 <div class="layout column-container">
     <div class="layout__item u-6/12 column u-12/12-palm column--red">
-        <div class="presentation-logo">
-            @include('partials.nav.visitors_nav')
-        </div>
         <h1 class="vertical-align-header">L’outil indispensable pour une gestion efficace des présences.</h1>
     </div>
     <div class="layout__item u-6/12 u-12/12-palm column column--blue">
