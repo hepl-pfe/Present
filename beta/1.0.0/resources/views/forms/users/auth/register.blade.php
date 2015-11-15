107b40ba15d67b3b72024c0c91cf0ec781e8a95f
Daniel <daniel.schreurs@hotmail.com>
2015-11-15 22:23:01 +0100

    Simplifier le formulaire d'inscription

--- beta/1.0.0/resources/views/forms/users/auth/register.blade.php
+++ beta/1.0.0/resources/views/forms/users/auth/register.blade.php
@@ -2,7 +2,6 @@
     {!! Form::label('first_name','Votre prénom',['class'=>'floating-placeholder__label floating-placeholder__label--blue']) !!}
     {!! Form::input('text','first_name',old('first_name'),['class'=>'floating-placeholder__input--huge floating-placeholder__input']) !!}
     @include('errors.error_field',['field'=>'first_name'])
-
 </div>
 <div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
     {!! Form::label('last_name','Votre nom de famille',['class'=>'floating-placeholder__label floating-placeholder__label--blue']) !!}
@@ -19,21 +18,11 @@
     {!! Form::label('password','Votre mot de passe',['class'=>'floating-placeholder__label floating-placeholder__label--blue']) !!}
     {!! Form::input('password','password',old('email'),['class'=>'oPasswordInput floating-placeholder__input--huge floating-placeholder__input']) !!}
     @include('errors.error_field',['field'=>'password'])
-
 </div>
 <div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
     {!! Html::link('#','Montrer le mot de passe',['class'=>'btn-link btn-link--white','id'=>'oLinkPassword']) !!}
 </div>
-<div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
-    {!! Form::label('school','institutions',['class'=>'floating-placeholder__label floating-placeholder__label--blue']) !!}
-    {!! Form::select('school',['le nom d’une seuper belle école']) !!}
-    @include('errors.error_field',['field'=>'school'])
-</div>
-<div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
-    {!! Form::label('more','Mon institution n’est pas encore référencée',['class'=>'']) !!}
-    {!! Form::checkbox('more',old('more'),false) !!}
-</div>
 {!! Form::submit('S’inscrire',['class'=>'btn  btn--white']) !!}
 <div class="form-group">
-{!! Html::link('auth/login', 'Je suis déjà membre', array('class' => 'btn-link btn-link--white')) !!}
+    {!! Html::link('auth/login', 'Je suis déjà membre', array('class' => 'btn-link btn-link--white')) !!}
 </div>
