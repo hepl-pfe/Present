54d02cbd7da8c261263fbf983f4f7246137898d3
Daniel <daniel.schreurs@hotmail.com>
2015-11-15 22:17:41 +0100

    fixe URL

--- beta/1.0.0/resources/views/auth/register.blade.php
+++ beta/1.0.0/resources/views/auth/register.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.master_layout')
 @section('content')
     @extends('layouts.welkocome_layout')
-    @section('form-content')
+@section('form-content')
     <h2>S’inscrire</h2>
-    {!! Form::open( ['url'=>'/auth/create']) !!}
+    {!! Form::open( array('action','Auth\AuthController@postRegister')) !!}
     @include('forms.users.auth.register')
     {!! Form::close() !!}
-    @endsection
+@endsection
 @endsection
