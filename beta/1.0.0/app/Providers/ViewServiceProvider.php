d662494555d19f329581bf31db8e1ef22fa7880f
Daniel <daniel.schreurs@hotmail.com>
2015-11-16 12:30:49 +0100

    update data

--- beta/1.0.0/app/Providers/ViewServiceProvider.php
+++ beta/1.0.0/app/Providers/ViewServiceProvider.php
@@ -1,39 +1,40 @@
 <?php
 
-namespace App\Providers;
-use App\School;
-use App\User;
-use View;
-use Illuminate\Support\ServiceProvider;
+    namespace App\Providers;
 
-class ViewServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        View::composer('partials.panel.teacher_controller', function($view)
-        {
-            $data['user']=\Auth::user();
-            $data['school']=\Auth::user()->school()->get()->first();
-            $view->with('userData',$data);
-        });
-        View::composer('schools.config', function($view)
-        {
-            $view->with('schools', School::all('name', 'id'));
-        });
-    }
+    use App\School;
+    use App\User;
+    use View;
+    use Illuminate\Support\ServiceProvider;
 
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
-    public function register()
+    class ViewServiceProvider extends ServiceProvider
     {
-        //
+        /**
+         * Bootstrap the application services.
+         *
+         * @return void
+         */
+        public function boot()
+        {
+            View::composer('layouts.teacher_layout', function ($view) {
+                $data['user'] = \Auth::user();
+                $data['school'] = \Auth::user()->school()->get()->first();
+                $view->with('userData', $data);
+            });
+            View::composer('schools.config', function ($view) {
+                $data['belongs_to_a_school']=false;
+                $data['all_schools']=School::all('name','slug','id');
+                $view->with('data',$data);
+            });
+        }
+
+        /**
+         * Register the application services.
+         *
+         * @return void
+         */
+        public function register()
+        {
+            //
+        }
     }
-}
--- beta/1.0.0/resources/views/schools/config.blade.php
+++ beta/1.0.0/resources/views/schools/config.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.teacher_layout')
 @section('teacher_content')
+<?php
+$schools=$data['all_schools'];
+?>
 <h1 class="big-page-header">Configuration de l’école</h1>
 <ul class="layout">
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
