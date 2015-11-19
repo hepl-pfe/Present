5631dbf9c6189fe1392eeaa2b15f31cc81991416
Daniel <daniel.schreurs@hotmail.com>
2015-11-19 21:47:54 +0100

    init colleagues list

--- beta/1.0.0/resources/views/teacher/teachers_index.blade.php
+++ beta/1.0.0/resources/views/teacher/teachers_index.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.teacher_layout')
 @section('teacher_content')
     <h1 class="big-page-header">Collègues</h1>
+    {!! Form::open([''=>'/']) !!}
+    <div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
+        {!! Form::label('school_id','Uniquement',['class'=>'floating-placeholder__label']) !!}
+        {!! Form::select('school_id',$schools->lists('name','id'),old('school_id'),['class'=>'floating-placeholder__input--huge floating-placeholder__input']) !!}
+        @include('errors.error_field',['field'=>'school_id'])
+    </div>
+    {!! Form::close() !!}
     @if(is_null($schools))
         <div class="informative-box">
             <p class="informative-box__text">Vous n’êtes pas encore <b>membre d'une école</b> ?
@@ -13,19 +20,20 @@
                 <p class="informative-box__text">Pas encore de <b>collègues</b>?  Demandez à vos collègues de venir <b>s'inscrire.</b></p>
             </div>
         @else
-            @foreach($schools as $school)
-                <h2>{!! $school->name !!}</h2>
+            <div class="cla">
                 <ul class="layout">
-                    @foreach($school->users as $user)
-                    <li class="profile-container layout__item u-2/12 u-2/12-desk u-3/12-lap u-6/12-palm">
-                        <a href="{!! URL::action('Www\UserController@show',['school_slug'=>$school->slug,'user_slug'=>$user->slug]) !!}" title="Renvoie vers la fiche de ">
-                            <img class="profile-picture user-image" src="./img/default_profile_picture.jpg" alt="">
-                            <span class="profile-name">{!! $user->first_name !!}&nbsp;{!! $user->last_name !!}</span>
-                        </a>
-                    </li>
+                    @foreach($schools as $school)
+                            @foreach($school->users as $user)
+                            <li class="layout__item u-2/12-desk u-6/12-lap u-12/12-palm">
+                                <a href="{!! URL::action('Www\UserController@show',['school_slug'=>$school->slug,'user_slug'=>$user->slug]) !!}" title="Renvoie vers la fiche de " class="media colleague__item">
+                                    <img class="user-image user-image--small media__img" src="./img/default_profile_picture.jpg" alt="">
+                                    <span class="media-body colleague__item__name">{!! $user->first_name !!}&nbsp;{!! $user->last_name !!}</span>
+                                </a>
+                            </li>
+                            @endforeach
                     @endforeach
                 </ul>
-            @endforeach
+            </div>
         @endif
     @endif
 @stop
