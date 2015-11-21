1b31c8b927f249d62cdbffefc0504a08b0f9a4ac
Daniel <daniel.schreurs@hotmail.com>
2015-11-21 13:06:19 +0100

    use the slug package

--- beta/1.0.0/app/School.php
+++ beta/1.0.0/app/School.php
@@ -3,10 +3,18 @@
     namespace App;
 
     use Illuminate\Database\Eloquent\Model;
-    use Illuminate\Support\Str;
+    use Cviebrock\EloquentSluggable\SluggableInterface;
+    use Cviebrock\EloquentSluggable\SluggableTrait;
 
-    class School extends Model
+    class School extends Model implements SluggableInterface
     {
+        use SluggableTrait;
+
+        protected $sluggable = [
+            'build_from' => 'name',
+            'save_to'    => 'slug',
+        ];
+
         /**
          * The database table used by the model.
          *
@@ -20,17 +28,6 @@
          * @var array
          */
         protected $fillable = ['name', 'slug'];
-
-        /**
-         * Create a slug when school is create
-         *
-         * @param $value
-         */
-        public function setNameAttribute($value)
-        {
-            $this->attributes['slug'] = Str::slug($value);
-            $this->attributes['name'] = $value;
-        }
 
         public function users()
         {
