4a560c9cfb88ae5c6d4236c31a038388cccaf87a
Daniel <daniel.schreurs@hotmail.com>
2015-11-28 16:06:04 +0100

    add link

--- beta/1.0.0/resources/views/modals/dashbord/cours.blade.php
+++ beta/1.0.0/resources/views/modals/dashbord/cours.blade.php
@@ -1,10 +1,17 @@
-<div class="box-header beta">Mes cours {!! link_to_action('Www\CoursController@create','Créer un cours !',[],['class'=>'btn btn--add btn--small btn--soft']) !!}</div>
+<div class="box-header beta">Mes
+    cours {!! link_to_action('Www\CoursController@create','Créer un cours !',[],['class'=>'btn btn--add btn--small btn--soft']) !!}</div>
 <ul class="box">
     @if(empty(\Auth::user()->cours->toArray()))
-        <li class="box__item">Vous n'avez pas encore de cours {!! link_to_action('Www\CoursController@create','Créer un cours !',[],['class'=>'']) !!}</li>
+        <li class="box__item">Vous n'avez pas encore de
+            cours {!! link_to_action('Www\CoursController@create','Créer un cours !',[],['class'=>'']) !!}</li>
     @else
-    @foreach(Auth::user()->cours as $cours)
-        <li class="box__item">{!! Html::linkAction('Www\CoursController@show',$cours->name,['slug'=>$cours->slug],['class'=>'']) !!}</li>
-    @endforeach
+        @foreach(Auth::user()->cours as $cours)
+            <li class="box__item">
+                    {!! Html::linkAction('Www\CoursController@show',$cours->name,['slug'=>$cours->slug],['class'=>'link--black link-spacer']) !!}
+                @if($cours->hasOccurrence)
+                    {!! link_to_action('Www\PresentController@getAllStudentfromOneOccurrence','Prendre les présences',['id'=>$cours->hasOccurrence],['class'=>'']) !!}
+                @endif
+            </li>
+        @endforeach
     @endif
 </ul>
--- beta/1.0.0/resources/views/teacher/dashboard.blade.php
+++ beta/1.0.0/resources/views/teacher/dashboard.blade.php
@@ -19,9 +19,6 @@
         @include('modals.dashbord.students')
     </li>
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
-        @include('modals.dashbord.take-present')
-    </li>
-    <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
         @include('modals.dashbord.colleagues')
     </li>
     <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
