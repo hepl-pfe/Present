966435d669cba5ba0bcba8639d760360b5c2a21d
Daniel <daniel.schreurs@hotmail.com>
2015-11-28 12:20:36 +0100

    ini Present controller

--- beta/1.0.0/app/Http/Controllers/Www/PresentController.php
+++ beta/1.0.0/app/Http/Controllers/Www/PresentController.php
@@ -1,90 +1,114 @@
 <?php
 
-namespace App\Http\Controllers\Www;
+    namespace App\Http\Controllers\Www;
 
-use App\Occurrence;
-use Illuminate\Http\Request;
+    use App\Cour;
+    use App\Occurrence;
+    use Illuminate\Http\Request;
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
+    use App\Http\Requests;
+    use App\Http\Controllers\Controller;
+    use Illuminate\Support\Facades\Auth;
 
-class PresentController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    class PresentController extends Controller
     {
-        $userOccurrences= \DB::table('occurrences')->whereIn('cour_id',\Auth::user()->cours()->lists('id'))->lists('id');
-        dd($userOccurrences);
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+        /**
+         * PresentController constructor.
+         */
+        public function __construct()
+        {
+            $this->middleware('auth');
+        }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        /**
+         * Display a listing of the resource.
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function index()
+        {
+            $occurrences = \Auth::user()->occurrences;
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+            return view('occurrences.index', compact('occurrences'));
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+        /**
+         * Show the form for creating a new resource.
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function create()
+        {
+            //
+        }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+        /**
+         * Store a newly created resource in storage.
+         *
+         * @param  \Illuminate\Http\Request $request
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function store(Request $request)
+        {
+            //
+        }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        /**
+         * Display the specified resource.
+         *
+         * @param  int $id
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function show($id)
+        {
+            //
+        }
+
+        /**
+         * Show the form for editing the specified resource.
+         *
+         * @param  int $id
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function edit($id)
+        {
+            //
+        }
+
+        /**
+         * Update the specified resource in storage.
+         *
+         * @param  \Illuminate\Http\Request $request
+         * @param  int                      $id
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function update(Request $request, $id)
+        {
+            //
+        }
+
+        /**
+         * Remove the specified resource from storage.
+         *
+         * @param  int $id
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function destroy($id)
+        {
+            //
+        }
+
+        public function getAllStudentfromOneOccurrence($id)
+        {
+            $cours_id = Occurrence::findOrfail($id)->cour_id;
+            $cour = Cour::findOrFail($cours_id);
+            $students= $cour->classes()->firstOrFail()->students;
+            return view('teacher.occurrences', compact('cour', 'students'));
+        }
     }
-}
