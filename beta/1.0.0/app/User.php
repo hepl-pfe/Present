c9759d800b8176cbd57f978837ed3a2593b85fb1
Daniel <daniel.schreurs@hotmail.com>
2015-11-29 13:10:47 +0100

    add geter

--- beta/1.0.0/app/User.php
+++ beta/1.0.0/app/User.php
@@ -72,6 +72,17 @@
             return $this->first_name . ' ' . $this->last_name;
         }
 
+
+        public function getHasOccurrenceAttribute()
+        {
+            return  ! is_null(Occurrence::all()->where('user_id',$this->id)->first());
+        }
+        public function getGetAllOccurrenceAttribute()
+        {
+            return  Occurrence::all()->where('user_id',$this->id);
+        }
+
+
         /**
          * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
          */
