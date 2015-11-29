087ac17b146a72cf0fb214419f1983f20585663a
Daniel <daniel.schreurs@hotmail.com>
2015-11-29 14:08:37 +0100

    fixe error

--- beta/1.0.0/database/migrations/2015_11_29_125707_create_notes_table.php
+++ beta/1.0.0/database/migrations/2015_11_29_125707_create_notes_table.php
@@ -14,9 +14,9 @@ class CreateNotesTable extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
-            $table->integer('student_id');
+            $table->integer('student_id')->unsigned();
             $table->foreign('student_id')->references('id')->on('students');
             $table->text('note');
             $table->timestamps();
