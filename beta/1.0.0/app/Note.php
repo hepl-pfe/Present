b95b033903e8f7c806e7235881a669c99780a853
Daniel <daniel.schreurs@hotmail.com>
2015-11-29 16:31:16 +0100

    relation for notes

--- beta/1.0.0/app/Note.php
+++ beta/1.0.0/app/Note.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
-    //
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'notes';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id', 'student_id', 'note'];
+
+    public function student()
+    {
+        return $this->belongsTo('App\Student');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\user');
+    }
+
 }
