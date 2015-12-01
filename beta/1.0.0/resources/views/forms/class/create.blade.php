2b80b25d8c611a7d0ee5949701da63c95848face
Daniel <daniel.schreurs@hotmail.com>
2015-12-01 09:13:34 +0100

    fixe condition

--- beta/1.0.0/resources/views/forms/class/create.blade.php
+++ beta/1.0.0/resources/views/forms/class/create.blade.php
@@ -1,4 +1,4 @@
-@unless(empty($students))
+@unless(empty($students->toArray()))
 <div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
     {!! Form::label('students_id','Le nom des classes',['class'=>'floating-placeholder__label']) !!}
     {!! Form::select('students_id[]',$students,old('students_id'),['class'=>'floating-placeholder__input--huge floating-placeholder__input','multiple']) !!}
