d39d4029f0cef0790b0d9acc44ab8efc5abf626b
Daniel <daniel.schreurs@hotmail.com>
2015-12-01 16:36:08 +0100

    write seeders

---
+++ beta/1.0.0/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+    use Illuminate\Database\Seeder;
+
+    class UsersTableSeeder extends Seeder
+    {
+        /**
+         * Run the database seeds.
+         *
+         * @return void
+         */
+        public function run()
+        {
+
+            DB::table('users')->insert([
+                'first_name' => 'Daniel',
+                'last_name'  => 'Schreurs',
+                'slug'       => 'daniel-schreurs',
+                'email'      => 'daniel.schreurs@hotmail.com',
+                'password'   => bcrypt('12345'),
+            ]);
+
+            /*$faker = \Faker\Factory::create('fr_BE');
+            foreach (range(1, 3) as $index) {
+                DB::table('users')->insert([
+                    'first_name' => $faker->firstNameMale,
+                    'last_name'  => $faker->lastName,
+                    'slug'       => str_random(56),
+                    'email'      => $faker->email,
+                    'password'   => bcrypt('12345'),
+                ]);
+            }*/
+
+        }
+    }
