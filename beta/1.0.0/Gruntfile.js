a55a11d52b89fb2367856a2ff1454c8a508ffa7c
Daniel <daniel.schreurs@hotmail.com>
2015-12-02 13:44:45 +0100

    add svg

---
+++ beta/1.0.0/Gruntfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+ grunt.initConfig({
+  svgstore: {
+  options: {
+    prefix : 'shape-', // This will prefix each <g> ID
+  },
+  default : {
+      files: {
+        'public/svg/svg/svg-defs.svg': ['./resources/svg/*.svg'],
+      }
+    }
+  }
+});
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-svgstore');
+  // Default task.
+  grunt.registerTask('default', ['svgstore']);
+
+};
