26caefabdd4822aaa3cea7d368f20ff03a5b9d39
Daniel <daniel.schreurs@hotmail.com>
2015-12-05 13:11:16 +0100

    update cours modale

--- beta/1.0.0/resources/views/modals/dashbord/cours.blade.php
+++ beta/1.0.0/resources/views/modals/dashbord/cours.blade.php
@@ -17,12 +17,28 @@
         <li class="box__item"><span class="link-spacer">Vous n'avez pas encore de
             cours</span> {!! link_to_action('Www\CoursController@create','Créer un cours !',[],['class'=>'']) !!}</li>
     @else
-        @foreach(Auth::user()->cours as $cours)
+        @foreach(Auth::user()->cours as $cour)
             <li class="box__item">
-                {!! Html::linkAction('Www\CoursController@show',$cours->name,['slug'=>$cours->slug],['class'=>'link--black link-spacer']) !!}
-                @if($cours->hasOccurrence)
-                    {!! link_to_action('Www\PresentController@getAllStudentfromOneOccurrence','Prendre les présences',['id'=>$cours->getOccurrence->id],['class'=>'']) !!}
+                {!! Html::linkAction('Www\CoursController@show',$cour->name,['slug'=>$cour->slug],['class'=>'link--black link-spacer']) !!}
+                @if($cour->hasOccurrence)
+                    {!! link_to_action('Www\PresentController@getAllStudentfromOneOccurrence','Prendre les présences',['id'=>$cour->getOccurrence->id],['class'=>'']) !!}
                 @endif
+                {!!  Form::open(['action' => ['Www\CoursController@destroy', $cour->id], 'method' => 'delete','class'=>'inline']) !!}
+                <button class="link--alert" class=""
+                        data-toggle="tooltip" title="Supprimer le cours : {!! $cour->name !!}">
+                    <svg class="svg-basic svg--alert">
+                        <use xlink:href="#shape-trash"></use>
+                    </svg>
+                    <span class="visuallyhidden">Supprimer le cours {!! $cour->name !!}</span>
+                </button>
+                {!! Form::close() !!}
+                <a href="{!! URL::action('Www\CoursController@edit',['slug'=>$cour->slug]) !!}" class=""
+                   data-toggle="tooltip" title="Modifier le cours : {!! $cour->name !!}">
+                    <svg class="svg-basic svg--blue">
+                        <use xlink:href="#shape-edit"></use>
+                    </svg>
+                    <span class="visuallyhidden">Modifier le cours {!! $cour->name !!}</span>
+                </a>
             </li>
         @endforeach
     @endif
