37db858087e75a49a47a4eacb50ec86b9fc7c1be
Daniel <daniel.schreurs@hotmail.com>
2015-12-09 12:02:47 +0100

    update view when teacher has no students

--- beta/1.0.0/resources/views/forms/class/add_student_to_school.blade.php
+++ beta/1.0.0/resources/views/forms/class/add_student_to_school.blade.php
@@ -1,8 +1,14 @@
-<div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
-    {!! Form::label('name','Le nom de la classe',['class'=>'floating-placeholder__label']) !!}
-    {!! Form::input('text','name',null,['class'=>'floating-placeholder__input--huge floating-placeholder__input']) !!}
-    @include('errors.error_field',['field'=>'name'])
-</div>
+@if(empty(Auth::user()->students->toArray()))
+    <p class="alert-neutre message-box">Oh vous ne semblez pas encore d’avoir des élèves. Vous pouvez charger un fichier <a href="#">.csv</a> pour
+        ajouter des élèves.</p>
+    <div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge add_student_to_class">
+        {!! Form::label('csv','Charger la liste de vos élèves',['class'=>'floating-placeholder__label']) !!}
+        {!! Form::input('file','csv',old('csv'),['class'=>'floating-placeholder__input--huge floating-placeholder__input']) !!}
+        @include('errors.error_field',['field'=>'csv'])
+    </div>
+@else
+    Lister les élèves
+@endif
 <div class="form-group">
     {!! Form::submit($submit,['class'=>'btn']) !!}
 </div>
