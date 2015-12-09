1f1f70022d936718ab960b9fe0b6d2846f8e3211
Daniel <daniel.schreurs@hotmail.com>
2015-12-09 22:21:36 +0100

    add student to classe

---
+++ beta/1.0.0/resources/views/forms/class/add_student_to_school.blade.php
@@ -0,0 +1,9 @@
+<p class="alert-neutre message-box">Ici Vous pouvez ajouter des élèves à cette classe. Entrez le nom d’un élève.</p>
+<div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge form-group--select">
+    {!! Form::label('students_id','Sélectionnez vos élèves',['class'=>'floating-placeholder__label']) !!}
+    {!! Form::select('students_id[]',$students,$selected_student,['class'=>'chosen-select floating-placeholder__input--huge floating-placeholder__input','multiple']) !!}
+    @include('errors.error_field',['field'=>'students_id'])
+</div>
+<div class="form-group">
+    {!! Form::submit($submit,['class'=>'btn']) !!}
+</div>
