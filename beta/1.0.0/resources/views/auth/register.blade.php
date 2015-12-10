98d42e44c24320488b7c5199d9944bba0c0861c9
Daniel <daniel.schreurs@hotmail.com>
2015-12-10 16:33:19 +0100

    update sing in view

--- beta/1.0.0/resources/views/auth/register.blade.php
+++ beta/1.0.0/resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master_layout')
 @section('content')
     <h1 class="visuallyhidden">Présent, l’outil indispensable pour une gestion efficace des présences.</h1>
-    <div class="welcome-log vertical-flex max-height aligner-item--midle-container">
+    <div class="welcome-container">
         {!! Form::open( ['action','Auth\AuthController@postRegister','class'=>'welcome-form']) !!}
             <h2>S’inscrire</h2>
             @include('forms.users.auth.register')
