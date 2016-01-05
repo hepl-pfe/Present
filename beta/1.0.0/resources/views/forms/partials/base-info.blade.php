ddf97c2abbc7c668bb73aa5fe7b1fd980abd0861
Daniel <daniel.schreurs@hotmail.com>
2016-01-05 19:00:30 +0100

    fixe remove modal

--- beta/1.0.0/resources/views/forms/partials/base-info.blade.php
+++ beta/1.0.0/resources/views/forms/partials/base-info.blade.php
@@ -1,4 +1,3 @@
 <div class="form-group__info-box">
-    <button type="button" class="close close--message-box" data-dismiss="alert" aria-hidden="true">&times;</button>
     <p class="form-group__info-box__text">{{ $message }}</p>
 </div>
