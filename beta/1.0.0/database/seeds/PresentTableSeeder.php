05364598fe064b781d3cf6d56f8a283b9dc5ce66
Daniel <daniel.schreurs@hotmail.com>
2016-01-15 15:49:50 +0100

    fixe present seeder

--- beta/1.0.0/database/seeds/PresentTableSeeder.php
+++ beta/1.0.0/database/seeds/PresentTableSeeder.php
@@ -15,13 +15,15 @@
             $iMaxOccurrence = \App\Occurrence::all()->count();
             $iMaxStudent = \App\Student::all()->count();
             $iOccurrences = 1;
-            $iStudents = 1;
-            while ($iOccurrences <= $iMaxOccurrence) {
-                while ($iStudents <= $iMaxStudent) {
+
+            // because while in while does not work
+            foreach (range(1, $iMaxOccurrence) as $index) {
+                $iStudents = 1;
+                foreach (range(1, $iMaxStudent) as $index) {
                     DB::table('presents')->insert([
                         'student_id'    => $iStudents,
                         'occurrence_id' => $iOccurrences,
-                        'is_present'    => $faker->numberBetween(0, 1),
+                        'is_present'    => 1,
                         'created_at'    => \Carbon\Carbon::now(),
                         'updated_at'    => \Carbon\Carbon::now()
                     ]);
