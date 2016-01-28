c0733ead41d89a7bcaa2a29c045f6b6373968124
Daniel <daniel.schreurs@hotmail.com>
2016-01-28 23:25:48 +0100

    add route

--- beta/1.0.0/app/Http/Routes/Www/WwwRoutes.php
+++ beta/1.0.0/app/Http/Routes/Www/WwwRoutes.php
@@ -47,6 +47,7 @@
     Route::get('auth/login', 'Auth\AuthController@getLogin');
     Route::post('auth/login', 'Auth\AuthController@postLogin');
     Route::get('auth/logout', 'Auth\AuthController@getLogout');
+    Route::get('login','AuthController@login');
 // Registration routes...
     Route::get('auth/register', 'Auth\AuthController@getRegister');
     Route::post('auth/register', 'Auth\AuthController@postRegister');
