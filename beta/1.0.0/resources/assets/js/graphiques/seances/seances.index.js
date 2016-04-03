b4f9e78bbf4172c8bbb9b0be5c2f992ac15ba967
Daniel <daniel.schreurs@hotmail.com>
2016-04-03 19:44:30 +0200

    update seances index

---
+++ beta/1.0.0/resources/assets/js/graphiques/seances/seances.index.js
@@ -0,0 +1,33 @@
+var ePiechartSeances = $( ".piechart-seances" );
+if ( ePiechartSeances.length ) {
+    google.charts.load( 'current', { 'packages': [ 'corechart' ] } );
+    ePiechartSeances.each(
+        function () {
+            var that = $( this );
+            google.charts.setOnLoadCallback( chartLoader );
+            function chartLoader() {
+                var aDataTable      = [ [ 'Task', '' ] ],
+                    oDataAttributes = that.data(),
+                    slices          = {};
+                for ( key in oDataAttributes ) {
+                    if ( oDataAttributes.hasOwnProperty( key ) ) {
+                        var aCurentData                = oDataAttributes[ key ].split( "," );
+                        aDataTable[ aCurentData[ 0 ] ] = [ aCurentData[ 1 ], parseInt( aCurentData[ 2 ] ) ];
+                        slices[ aCurentData[ 0 ] ] = { 'color': aCurentData[ 3 ] }
+                    }
+                }
+                console.log( slices );
+                var data    = google.visualization.arrayToDataTable( aDataTable );
+                var options = {
+                    'tooltip': { 'isHtml': true },
+                    slices
+
+                };
+
+                var chart = new google.visualization.PieChart( that.get( 0 ) );
+                chart.draw( data, options );
+            }
+        }
+    );
+
+}
