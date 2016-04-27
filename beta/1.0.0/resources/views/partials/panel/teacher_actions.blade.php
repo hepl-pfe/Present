7e4a2d6b13c197bceea8a8533dd6275d3899fa66
Daniel <daniel.schreurs@hotmail.com>
2016-04-27 15:38:10 +0200

    update dropdown

--- beta/1.0.0/resources/views/partials/panel/teacher_actions.blade.php
+++ beta/1.0.0/resources/views/partials/panel/teacher_actions.blade.php
@@ -11,34 +11,42 @@
         <div class="media dropdown-menu-container dropdown-menu-container--hover"
              data-intro="Retrouver ici les informations relatives à votre compte" data-step="1">
             <img src="{!! !empty($user->avatar)?'/image/user/40/40/'.$user->avatar:asset('./img/default_profile_picture.jpg') !!}" alt=""
-                 class="media__img user-image user-image--small">
+                 class="media__img user-image user-image--small user-image--teacher-action" width="40" height="40">
             <a href="" class="media__body header-meta no-underline">
                 <span class="header-meta__item">{!! $user->name !!}</span>
-                <svg class="svg-basic dropdown-menu__arraw">
+                <svg class="svg-basic svg--blue dropdown-menu__arraw">
                     <title>flèche vers le bas</title>
-                    <use xlink:href="#down"></use>
+                    <use xlink:href="#shape-down"></use>
                 </svg>
             </a>
             <div class="dropdown-menu">
                 <ul>
                     <li class="dropdown-menu__item">
+                        <svg class="svg-basic svg--blue dropdown-menu__arraw">
+                            <use xlink:href="#shape-power"></use>
+                        </svg>
                         {!! Html::link('auth/logout','Se déconnecter') !!}
                     </li>
                     @if($user->schools->first())
                         @if(count($user->schools)>1)
                             <li class="dropdown-menu__item">
+                                <svg class="svg-basic svg--blue dropdown-menu__arraw">
+                                    <use xlink:href="#shape-school"></use>
+                                </svg>
                                 {!! Html::linkAction('Www\SchoolController@index','Mes écoles') !!}
                             </li>
                         @else
                             <li class="dropdown-menu__item">
+                                <svg class="svg-basic svg--blue dropdown-menu__arraw">
+                                    <use xlink:href="#shape-school"></use>
+                                </svg>
                                 {!! Html::linkAction('Www\SchoolController@index','Mon école') !!}
                             </li>
                         @endif
                     @endif
                     <li class="dropdown-menu__item">
-                        <svg class="svg-basic dropdown-menu__arraw">
-                            <title>Configuration</title>
-                            <use xlink:href="#shape-compte"></use>
+                        <svg class="svg-basic svg--blue dropdown-menu__arraw">
+                            <use xlink:href="#shape-personal-settings"></use>
                         </svg>
                         <a href="{!! URL::action('Www\UserController@getConfig') !!}">Mon compte</a>
                     </li>
