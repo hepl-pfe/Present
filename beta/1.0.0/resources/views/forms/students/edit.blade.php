54b515de11bdb991f509f9f77e88c7f91e1f45ea
Daniel <daniel.schreurs@hotmail.com>
2016-05-25 18:12:54 +0200

    update edite form

--- beta/1.0.0/resources/views/forms/students/edit.blade.php
+++ beta/1.0.0/resources/views/forms/students/edit.blade.php
@@ -1,16 +1,16 @@
 <div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
     <label for="edit-student-first_name-{!! $student->id !!}" class="floating-placeholder__label">Prénom @include('forms.partials.required')</label>
-    {!! Form::input('text','first_name',old('first_name'),['class'=>'floating-placeholder__input--huge floating-placeholder__input','placeholder'=>'ex : Valérie','id'=>'edit-student-first_name-'.$student->id]) !!}
+    {!! Form::input('text','first_name',null,['class'=>'floating-placeholder__input--huge floating-placeholder__input','placeholder'=>'ex : Valérie','id'=>'edit-student-first_name-'.$student->id]) !!}
     @include('errors.error_field',['field'=>'first_name'])
 </div>
 <div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
     <label for="edit-student-last_name-{!! $student->id !!}" class="floating-placeholder__label">Nom de famille @include('forms.partials.required')</label>
-    {!! Form::input('text','last_name',old('last_name'),['class'=>'floating-placeholder__input--huge floating-placeholder__input','placeholder'=>'ex : Dumont','id'=>'edit-student-last_name-'.$student->id]) !!}
+    {!! Form::input('text','last_name',null,['class'=>'floating-placeholder__input--huge floating-placeholder__input','placeholder'=>'ex : Dumont','id'=>'edit-student-last_name-'.$student->id]) !!}
     @include('errors.error_field',['field'=>'last_name'])
 </div>
 <div class="floating-placeholder form-group floating-placeholder-float--blue floating-placeholder-float--huge">
     <label for="edit-student-email-{!! $student->id !!}" class="floating-placeholder__label">Email de l’élève @include('forms.partials.required')</label>
-    {!! Form::input('email','email',old('email'),['class'=>'floating-placeholder__input--huge floating-placeholder__input','placeholder'=>'ex : velérie.dumont@domaine.com','id'=>'edit-student-email-'.$student->id]) !!}
+    {!! Form::input('email','email',null,['class'=>'floating-placeholder__input--huge floating-placeholder__input','placeholder'=>'ex : velérie.dumont@domaine.com','id'=>'edit-student-email-'.$student->id]) !!}
     @include('errors.error_field',['field'=>'email'])
 </div>
 <div class="form-group">
