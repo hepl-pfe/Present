c5f77ef8909652705ddd67947aaed3bdf30e2515
Daniel <daniel.schreurs@hotmail.com>
2016-05-31 23:28:23 +0200

    fixed case

--- beta/1.0.0/resources/views/partials/nav/teacher_nav.blade.php
+++ beta/1.0.0/resources/views/partials/nav/teacher_nav.blade.php
@@ -11,28 +11,28 @@
         <svg class="svg-basic svg-nav">
             <use xlink:href="#shape-cours"></use>
         </svg>
-        <span class="navbar-item"><span class="visuallyhidden--palm">Mes</span> <span class="case visuallyhidden--lap">C</span><span class="case visuallyhidden--palm visuallyhidden--lap-and-up">c</span>ours</span>
+        <span class="navbar-item"><span class="visuallyhidden--palm">Mes</span> <span class="case visuallyhidden--lap visuallyhidden--lap-and-up">C</span><span class="case visuallyhidden--palm ">c</span>ours</span>
     </a>
     <a href="{!! URL::action('Www\StudentController@index') !!}"
        class="main-nav__item {{ $nav=='students' ? 'main-nav__item--active' : '' }}">
         <svg class="svg-basic svg-nav">
             <use xlink:href="#shape-students"></use>
         </svg>
-        <span class="navbar-item"><span class="visuallyhidden--palm">Mes</span> <span class="case visuallyhidden--lap">É</span><span class="case visuallyhidden--palm visuallyhidden--lap-and-up">é</span>lèves</span>
+        <span class="navbar-item"><span class="visuallyhidden--palm">Mes</span> <span class="case visuallyhidden--lap visuallyhidden--lap-and-up">É</span><span class="case visuallyhidden--palm">é</span>lèves</span>
     </a>
     <a href="{!! URL::action('Www\ClassController@index') !!}"
        class="main-nav__item {{ $nav=='classes' ? 'main-nav__item--active' : '' }}">
         <svg class="svg-basic svg-nav">
             <use xlink:href="#shape-classes"></use>
         </svg>
-        <span class="navbar-item"><span class="visuallyhidden--palm">Mes</span> <span class="case visuallyhidden--lap">C</span><span class="case visuallyhidden--palm visuallyhidden--lap-and-up">c</span>lasses</span>
+        <span class="navbar-item"><span class="visuallyhidden--palm">Mes</span> <span class="case visuallyhidden--lap visuallyhidden--lap-and-up">C</span><span class="case visuallyhidden--palm">c</span>lasses</span>
     </a>
     <a href="{!! URL::action('Www\PresentController@index') !!}"
        class="main-nav__item {{ $nav=='seances' ? 'main-nav__item--active' : '' }}">
         <svg class="svg-basic svg-nav">
             <use xlink:href="#shape-calendar"></use>
         </svg>
-        <span class="navbar-item"><span class="visuallyhidden--palm">Mes</span> <span class="case visuallyhidden--lap">S</span><span class="case visuallyhidden--palm visuallyhidden--lap-and-up">s</span>éances</span>
+        <span class="navbar-item"><span class="visuallyhidden--palm">Mes</span> <span class="case visuallyhidden--lap visuallyhidden--lap-and-up">S</span><span class="case visuallyhidden--palm ">s</span>éances</span>
     </a>
 {{--     <a href="{!! URL::action('Www\SchoolController@index') !!}"
        class="visuallyhidden--palm main-nav__item {{ $nav=='schools' ? 'main-nav__item--active' : '' }}">
