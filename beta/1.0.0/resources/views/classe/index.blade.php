5948165a27b237170bf2b05c7d163612e74c9117
Daniel <daniel.schreurs@hotmail.com>
2016-06-01 12:31:25 +0200

    init filter on index

--- beta/1.0.0/resources/views/classe/index.blade.php
+++ beta/1.0.0/resources/views/classe/index.blade.php
@@ -2,19 +2,19 @@
 @section('title', 'Mes classes')
 @section('teacher_content')
     @include('partials.panel.index_actions')
-    <ul class="layout">
-        @if($classes->count()<1)
-            <div class="layout__item u-6/12-desk u-12/12-lap u-12/12-palm">
-                @include('forms.partials.base-info--important',['message'=>'C’est ici que vous retrouverez toutes vos classes.'])
-            </div>
-        @else
-            @include('forms.filter.indexView.filterClasse')
+    @if($classes->count()<1)
+        <div class="layout__item u-6/12-desk u-12/12-lap u-12/12-palm">
+            @include('forms.partials.base-info--important',['message'=>'C’est ici que vous retrouverez toutes vos classes.'])
+        </div>
+    @else
+        @include('forms.filter.indexView.filterClasse')
+        <ul class="layout">
             @foreach($classes as $classe)
                 <li class="box-container layout__item u-4/12-desk u-6/12-lap u-12/12-palm">
                     @include('modals.classes.one-classe')
                 </li>
             @endforeach
-        @endif
-    </ul>
+        </ul>
+    @endif
     @include('pagination.default', ['paginator' => $classes])
 @stop
