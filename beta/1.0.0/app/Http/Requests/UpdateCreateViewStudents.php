fc3f8e8925dfb285eb875bfac40185efc11fe80e
Daniel <daniel.schreurs@hotmail.com>
2016-06-02 23:16:17 +0200

    update request

--- beta/1.0.0/app/Http/Requests/UpdateCreateViewStudents.php
+++ beta/1.0.0/app/Http/Requests/UpdateCreateViewStudents.php
@@ -25,14 +25,13 @@
         public function rules()
         {
             $id = \Auth::user()->id;
-
             return [
                 'create_view_student_list_block'     => 'boolean',
                 'create_view_classe_list_block'      => 'boolean',
                 'create_view_cours_list_block'       => 'boolean',
-                'create_view_student_nbr_pagination' => 'integer|min:1|max:10',
-                'create_view_classe_nbr_pagination'  => 'integer|min:1|max:10',
-                'create_view_cours_nbr_pagination'   => 'integer|min:1|max:10',
+                'create_view_student_nbr_pagination' => 'integer|min:'.min(config('app.defaultCreatePaginationStudent')).'|max:'.max(config('app.defaultCreatePaginationStudent')),
+                'create_view_classe_nbr_pagination'  => 'integer|min:'.min(config('app.defaultCreatePaginationClasse')).'|max:'.max(config('app.defaultCreatePaginationClasse')),
+                'create_view_cours_nbr_pagination'   => 'integer|min:'.min(config('app.defaultCreatePaginationCours')).'|max:'.max(config('app.defaultCreatePaginationCours')),
                 'create_view_student_classe_id'      => 'integer|exists:classes,id,user_id,' . $id
             ];
         }
