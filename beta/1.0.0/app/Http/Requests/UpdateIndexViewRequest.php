5dad5e1d399fa847f86efe46de5408d13ad84c6e
Daniel <daniel.schreurs@hotmail.com>
2016-06-02 23:16:28 +0200

    uddate request

--- beta/1.0.0/app/Http/Requests/UpdateIndexViewRequest.php
+++ beta/1.0.0/app/Http/Requests/UpdateIndexViewRequest.php
@@ -24,14 +24,13 @@
         public function rules()
         {
             $id = \Auth::user()->id;
-
             return [
                 'index_view_student_list_block'     => 'boolean',
                 'index_view_classe_list_block'      => 'boolean',
                 'index_view_cours_list_block'       => 'boolean',
-                'index_view_student_nbr_pagination' => 'integer|min:1|max:10',
-                'index_view_classe_nbr_pagination'  => 'integer|min:1|max:10',
-                'index_view_cours_nbr_pagination'   => 'integer|min:1|max:10',
+                'index_view_student_nbr_pagination' => 'integer|min:'.min(config('app.defaultIndexPaginationStudent')).'|max:'.max(config('app.defaultIndexPaginationStudent')),
+                'index_view_classe_nbr_pagination'  => 'integer|min:'.min(config('app.defaultIndexPaginationClasse')).'|max:'.max(config('app.defaultIndexPaginationClasse')),
+                'index_view_cours_nbr_pagination'   => 'integer|min:'.min(config('app.defaultIndexPaginationCours')).'|max:'.max(config('app.defaultIndexPaginationCours')),
                 'index_view_student_classe_id'      => 'integer|exists:classes,id,user_id,' . $id
             ];
         }
