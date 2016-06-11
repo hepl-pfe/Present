c23abbb41b7218e83fa51cfd60c84ccb483d339e
Daniel <daniel.schreurs@hotmail.com>
2016-06-11 15:07:20 +0200

    update seeders

--- beta/1.0.0/database/seeds/PresentTableSeeder.php
+++ beta/1.0.0/database/seeds/PresentTableSeeder.php
@@ -15,19 +15,17 @@
             $iMaxOccurrence = \App\Occurrence::all()->count();
             $iMaxStudent = \App\Student::all()->count();
             $iOccurrences = 1;
-
-            // because while in while does not work
             foreach (range(1, $iMaxOccurrence) as $index) {
-                $iStudents = 1;
-                foreach (range(1, $iMaxStudent) as $index) {
-                    DB::table('presents')->insert([
-                        'student_id'    => $iStudents,
-                        'occurrence_id' => $iOccurrences,
-                        'statut_id'    => $faker->numberBetween($min = 1, $max = 3),
-                        'created_at'    => \Carbon\Carbon::now(),
-                        'updated_at'    => \Carbon\Carbon::now()
-                    ]);
-                    $iStudents++;
+                foreach (\App\Occurrence::findOrFail($iOccurrences)->classe()->get() as $classe) {
+                    foreach ($classe->students as $student) {
+                        DB::table('presents')->insert([
+                            'student_id'    => $student->id,
+                            'occurrence_id' => $iOccurrences,
+                            'statut_id'     => $faker->numberBetween($min = 1, $max = 3),
+                            'created_at'    => \Carbon\Carbon::now(),
+                            'updated_at'    => \Carbon\Carbon::now()
+                        ]);
+                    }
                 }
                 ++$iOccurrences;
             }
